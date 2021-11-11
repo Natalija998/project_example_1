2da26b29ea4760a25e332f9286f31ca71dd19b79
NatalijaMilosevic <natalijamil@elfak.rs>
2021-11-11 23:47:45 +0100

    Kreiranje poruke i aktora. Slanje/primanje poruke.

---
+++ Actor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Akka.Actor;
+
+namespace project_example_1
+{
+    class Actor : ReceiveActor
+    {
+        public Actor()
+        {
+            Receive<Message>(x => Console.WriteLine("Actor received message: {0}",x.Poruka));
+        }
+    }
+}
---
+++ Message.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace project_example_1
+{
+    class Message
+    {
+        public string Poruka { get; private set; }
+
+        public Message(string poruka)
+        {
+            Poruka = poruka;
+        }
+    }
+}
